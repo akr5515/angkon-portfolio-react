4cf851ff523b8bc4a0b7491c2763470380bc86b3
akr5515 <akrangkon5515@gmail.com>
2023-08-27 03:25:18 +0600

    fix: data modified

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,21 +1,21 @@
 export const educationData = [
   {
     id: 1,
-    institution: "Varsity",
-    course: "Bachelor Degree",
+    institution: "Chittagong University of Engineering & Technology",
+    course: "B.Sc(CSE)",
     startYear: "2016",
     endYear: "2021",
   },
   {
     id: 2,
-    institution: "College",
+    institution: "Rangpur Govt. College",
     course: "Higher Secondary",
     startYear: "2013",
     endYear: "2015",
   },
   {
     id: 3,
-    institution: "School",
+    institution: "Rangpur Zilla School",
     course: "Secondary School",
     startYear: "2005",
     endYear: "2013",
